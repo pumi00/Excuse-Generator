bb5483c39d433a88dcbefe0c9b3350c7f152d6cb
pumi00 <pumi0751@gmail.com>
2024-08-09 11:07:18 +0100

    Finish the project

--- excusefunction.js
+++ excusefunction.js
@@ -1,7 +1,21 @@
-const colors =[ "red", "blue", "orange", "purple", "yellow", "green"];
-function cambiarColor() {
-    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.getElementById("texto").style.color = colorAleatorio;
+let who = ["cat", 'My Grandma', 'My Dad', 'bird', 'tiger'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when i was sleeping', 'while I was exercising', 'during my luch', 'while I was praying'];
+
+function getRandomNumber(a,b){
+    return Math.floor(Math.random() * b-a) + a;
 }
 
-setInterval(cambiarColor, 500);
+function getRandom(anyArray){
+    let max = who.length;
+    let min = 0;
+    console.log(max, min, anyArray)
+    let random = getRandomNumber(min, max);
+    return anyArray[random];
+}
+
+window.onload = function(){
+    console.log(who);
+    document.querySelector('#excuse').innerHTML = getRandom(who)+ ' ' + getRandom(action) + ' ' + getRandom(what) + ' ' + getRandom(when)
+};
